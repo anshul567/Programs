350e77e6d4ed68767d8e16030d7f58aab570c7f9
Anshul Dhanotiya <anshuldhanotiya0@gmail.com>
2020-03-29 16:33:00 +0530

    Add files via upload

---
+++ 100 Programs/50. Employee's gross salary.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+struct Employee
+	{
+		char empname[24];
+		float basicpay,da;
+	};
+int main()
+{
+	int i;
+	struct Employee e[5];
+	printf("Enter employees name and basic pay\n");
+	for(i=1;i<=5;i++)
+	{
+		printf("Enter details of %d employee\n",i);
+	    scanf("%s%f",&e[i].empname,&e[i].basicpay);
+	    e[i].da=e[i].basicpay*0.52;
+    }
+    printf("________________________________________________________________________________________________________________________\n");
+	printf("Employee's Name \t Gross Salary\n");
+	printf("________________________________________________________________________________________________________________________\n");
+	for(i=1;i<=5;i++)
+	printf("%s\t\t\t%f\n",e[i].empname,e[i].basicpay+e[i].da);
+	return 0;
+}
