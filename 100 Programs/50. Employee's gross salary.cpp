49033664ef07827caeb6ac20f14d37063b4f3bee
Anshul Dhanotiya <anshuldhanotiya0@gmail.com>
2020-03-29 17:50:05 +0530

    Update 50. Employee's gross salary.cpp

--- 100 Programs/50. Employee's gross salary.cpp
+++ 100 Programs/50. Employee's gross salary.cpp
@@ -8,9 +8,9 @@ struct Employee
 int main()
 {
 	int i;
-	struct Employee e[5];
+	struct Employee e[100];
 	printf("Enter employees name and basic pay\n");
-	for(i=1;i<=5;i++)
+	for(i=1;i<=100;i++)
 	{
 		printf("Enter details of %d employee\n",i);
 	    scanf("%s%f",&e[i].empname,&e[i].basicpay);
@@ -19,7 +19,7 @@ int main()
     printf("________________________________________________________________________________________________________________________\n");
 	printf("Employee's Name \t Gross Salary\n");
 	printf("________________________________________________________________________________________________________________________\n");
-	for(i=1;i<=5;i++)
+	for(i=1;i<=100;i++)
 	printf("%s\t\t\t%f\n",e[i].empname,e[i].basicpay+e[i].da);
 	return 0;
 }
