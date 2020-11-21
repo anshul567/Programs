d49b428185cca55e3bc3f7933255800267283700
Anshul Dhanotiya <anshuldhanotiya0@gmail.com>
2020-11-21 19:45:49 +0530

    Update 17. Leap year or not.cpp

--- 100 Programs/17. Leap year or not.cpp
+++ 100 Programs/17. Leap year or not.cpp
@@ -5,7 +5,7 @@ main()
 	int y;
 	cout<<"Enter year\n";
 	cin>>y;
-	if(y%4==0 || (y%400==0 && y%100==0))
+	if(y%400==0 || (y%4==0 && y%100!=0))
 	cout<<"it's a leap year\n";
 	else
 	cout<<"Not a leap year";
